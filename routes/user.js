cd9eaf8e1b0f433fa0d8eec5644055d9b0947ca5
William Norman <lordcrekit@gmail.com>
2016-11-07 19:13:16 -0500

    Added template routes for user stuff.

---
+++ routes/user.js
@@ -0,0 +1,2 @@
+// Route for dealing with individual users. This would be their account page,
+//  for example.
---
+++ routes/users.js
@@ -0,0 +1,1 @@
+// Rout for userbase information. Also responsible for creating new users.
